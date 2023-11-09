5d6878baa7a18ac0deae5466af7b3cd64c0f717d
icecream0910 <taein2370@naver.com>
2023-11-10 00:21:50 +0900

    UPDATE: open graph tag 추가

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,20 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: '태인 @icecream',
-  description: '새로움에 끊임없이 도전하는 고등학생 개발자입니다',
+  description: '새로움에 끊임없이 도전하는 개발자입니다',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <head>
+        <meta property="og:title" content="태인 @icecream" />
+        <meta property="og:site_name" content="태인 @icecream" />
+        <meta property="og:url" content="yuntae.in" />
+        <meta property="og:description" content="새로움에 끊임없이 도전하는 개발자입니다" />
+        <meta property="og:type" content="profile" />
+        <meta property="og:image" content="/og_thumbnail.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
